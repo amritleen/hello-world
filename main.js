1237c6d7776c941110fcf8b977694bc99766a119
amritleen <36210098+amritleen@users.noreply.github.com>
2018-09-04 13:20:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
   //      showLegend = false;    
   console.log("im in js file");
 });
-      function onSignIn(googleUser) {
-        
-        alert('in onSignIn');
-        debugger;  
-        console.log('Logged in as: ' + googleUser.getBasicProfile().getEmail());
-    }
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
